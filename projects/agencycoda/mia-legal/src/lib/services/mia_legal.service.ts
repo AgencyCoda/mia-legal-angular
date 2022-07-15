c9526e84f9024bb5c1ae43f23a1b9416609e1829
Matias Camiletti <matias.camiletti@gmail.com>
2022-07-15 10:38:00 -0300

    Fixes to Angular 13.

--- projects/agencycoda/mia-legal/src/lib/services/mia_legal.service.ts
+++ projects/agencycoda/mia-legal/src/lib/services/mia_legal.service.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@angular/core';
 import { MiaLegal } from '../entities/mia_legal';
-import { MiaBaseCrudHttpService } from '@agencycoda/mia-core';
+import { MiaBaseCrudHttpService, MiaCoreConfig } from '@agencycoda/mia-core';
 import { HttpClient } from '@angular/common/http';
 import { MiaAuthConfig, MIA_AUTH_PROVIDER } from '@agencycoda/mia-auth';
 
@@ -11,9 +11,9 @@ export class MiaLegalService extends MiaBaseCrudHttpService<MiaLegal> {
 
   constructor(
     protected http: HttpClient,
-    @Inject(MIA_AUTH_PROVIDER) protected config: MiaAuthConfig,
+    @Inject(MIA_AUTH_PROVIDER) protected config: MiaCoreConfig,
   ) {
-    super(http);
+    super(config, http);
     this.basePathUrl = config.baseUrl + 'mia-legal';
   }
  
